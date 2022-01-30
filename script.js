12a75aba5782ad56370216e8cd8e43a5dca117a1
Katie Pestotnik <katie.pestotnik@gmail.com>
2022-01-30 14:55:42 -0700

    review

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function miniMaxSum(arr) {
     console.log(min + ' ' + max)
     
 }
-miniMaxSum([2, 4, 6, 1, 7, 2])
+// miniMaxSum([2, 4, 6, 1, 7, 2])
 
 function plusMinus(arr) {
     // Write your code here
@@ -31,4 +31,46 @@ function plusMinus(arr) {
     
 }
 
-plusMinus([1,1,0,-1,-1])
+// plusMinus([1, 1, 0, -1, -1])
+
+// Sample Input
+
+// 07:05:45PM
+// Sample Output
+
+// 19:05:45
+
+
+
+
+function pillars(num_pill, dist, width) {
+    // your code here
+    const centDistance = (((num_pill * dist) * 100))
+    const actual = centDistance - (width *2)
+    return actual
+}
+  
+// number of pillars (≥ 1);
+// distance between pillars (10 - 30 meters);
+// width of the pillar (10 - 50 centimeters).
+// Calculate the distance between the first and the last pillar in centimeters (without the width of the first and last pillar).
+
+console.log(pillars(2, 20, 25))
+//2000
+
+function digitize(n) {
+const arr = Array.from(String(n), Number).reverse()
+
+}
+  
+console.log(digitize(348597))
+
+function domainName(url){
+    const shorten = url.split()
+    const remove = shorten.slice(0, 6)
+
+}
+  
+domainName("http://github.com/carbonfive/raygun")
+//"github"
+
